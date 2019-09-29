cb4d619e4d0deefaf46b9777997badd23894c0e8
AirMaxSys <gy520813@live.ocm>
2019-09-30 00:01:08 +0800

    2019.9.29: Bubble Sort

--- algorithm/sort/bubble.py
+++ algorithm/sort/bubble.py
@@ -10,11 +10,9 @@ def Bubble(a):
     for i in range( len(a) ):
         # for j in range( -1, i-len(a), -1):
         for j in range( len(a) - 1, i, -1):
-            # print("%2d " % a[j] , end='')
             if(a[j] > a[j - 1]):
                 # Exchange
                 a[j], a[j - 1] = a[j - 1], a[j]
-        # print()
 
 a = [12, 3, 6, 7, 0, 23]
 Bubble(a)
