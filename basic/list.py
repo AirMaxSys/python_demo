8e31651df56c4609a0a83bfb16ae55d40a4c1e17
AirMaxSys <gy520813@live.com>
2020-01-01 17:15:36 +0800

    update

--- basic/list.py
+++ basic/list.py
@@ -11,15 +11,33 @@ a[-1] = 10
 print(a)
 
 # 列表基本操作：
+
 #   +:连接两个列表
 print(a + ['h','e','l','l','o'])
+
 #   *:列表重复N次
 print(a*2)
+
 #   len():列表中元素的个数
 print("len(a[2:]) =",len(a[2:]))
+
 #   for循环遍历列表
 for var in a:
     print(var, end='')
 print()
+
 #   <expr> in <list>:检查<expr>是否在列表中
 print("hello" in a)
+
+# 'not in' list
+pet = ['cat', 'pig', 'dog', 'parrot']
+if 'parrot' in pet:
+    print('Parrot is a kind of pet.')
+else if 'tiger' not in pet:
+    print('Tiger is not a kind of pet.')
+
+# 多重赋值技巧(注意：等号左边的变量数目必须要和列表元素数目一致)
+num = [0x10, 0x20, 0x30, 0x40, 0x50]
+num1, num2, num3, num4, num5 = num
+print(num1, num2, num3, num4, num5)
+
