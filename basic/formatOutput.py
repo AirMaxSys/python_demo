c3d04504a04c457c38fb8265d5a6a5bb4a4dd114
AirMaxSys <gy520813@live.com>
2020-07-31 09:01:01 +0800

    update format output

---
+++ basic/formatOutput.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# python格式话输出
+def main():
+    a, b = 10, 20
+    print('%d * %d = %d' % (a, b, (a * b)))
+
+    s1 = 'Hello'
+    name1='john'
+    print('{0}, {1}'.format(s1, name1))
+
+    # after python3.6
+    print(f'{s1} {name1}, you are nice')
+
+if __name__ == "__main__":
+    main()
